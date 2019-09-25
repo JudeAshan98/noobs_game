82284c6c5a0b3d194b56d4713d93cdfb6dc4f508
https://github.com/JudeAshan98/Login_SQLite.git <judeashanlakmal356@gmail.com>
2019-09-25 11:45:41 +0530

    seventh commit Jude

--- app/src/main/java/com/example/BrianStrom/bonus_game.java
+++ app/src/main/java/com/example/BrianStrom/bonus_game.java
@@ -1,13 +1,33 @@
 package com.example.BrianStrom;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class bonus_game extends AppCompatActivity {
 
+    Button answer1 , answer2,answer3,answer4;
+    TextView score , questoin;
     public String EEt_extra;
+
+    private questions mQuestions = new questions();
+
+    private String mAnswer;
+    private int mScore = 0;
+
+    private int mQuestionsLength = mQuestions.mQuestions.length;
+
+    Random r;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,5 +36,132 @@ public class bonus_game extends AppCompatActivity {
         Intent testInt = new Intent();
         EEt_extra = testInt.getStringExtra("test");
 
+
+        r = new Random();
+
+
+        answer1 = (Button)findViewById(R.id.answer1);
+        answer2 = (Button)findViewById(R.id.answer2);
+        answer3 = (Button)findViewById(R.id.answer3);
+        answer4 = (Button)findViewById(R.id.answer4);
+
+        score = (TextView)findViewById(R.id.score);
+        questoin= (TextView)findViewById(R.id.question);
+
+        score.setText("Score : " +mScore);
+
+        updateQuestoin(r.nextInt(mQuestionsLength));
+
+
+        answer1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(answer1.getText()==mAnswer){
+                    mScore = mScore + 10;
+                    score.setText("Score : " +mScore);
+                   updateQuestoin(r.nextInt(mQuestionsLength));
+                }else{
+                    wrongAnswer();
+                    updateQuestoin(r.nextInt(mQuestionsLength));
+                }
+
+            }
+        });
+
+        answer2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(answer2.getText()==mAnswer){
+                    mScore = mScore + 10;
+                    score.setText("Score : " +mScore);
+                    updateQuestoin(r.nextInt(mQuestionsLength));
+                }else{
+                    wrongAnswer();
+                    updateQuestoin(r.nextInt(mQuestionsLength));
+                }
+            }
+        });
+
+        answer3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(answer3.getText()==mAnswer){
+                    mScore = mScore + 10;
+                    score.setText("Score : " +mScore);
+                    updateQuestoin(r.nextInt(mQuestionsLength));
+                }else{
+                    wrongAnswer();
+                    updateQuestoin(r.nextInt(mQuestionsLength));
+                }
+            }
+        });
+
+        answer4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(answer4.getText()==mAnswer){
+                    mScore = mScore + 10;
+                    score.setText("Score : " +mScore);
+                    updateQuestoin(r.nextInt(mQuestionsLength));
+                }else{
+                    wrongAnswer();
+                    updateQuestoin(r.nextInt(mQuestionsLength));
+                }
+            }
+        });
+    }
+
+    private void updateQuestoin(int qno){
+        questoin.setText(mQuestions.getQuestion(qno));
+        answer1.setText(mQuestions.getChoice1(qno));
+        answer2.setText(mQuestions.getChoice2(qno));
+        answer3.setText(mQuestions.getChoice3(qno));
+        answer4.setText(mQuestions.getChoice4(qno));
+
+        mAnswer = mQuestions.getCorrectAnswer(qno);
+
+    }
+
+    private void wrongAnswer(){
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(bonus_game.this);
+        alertDialogBuilder
+                .setMessage("Invalid Answer !")
+                .setCancelable(false)
+                .setPositiveButton("Ok",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int i) {
+                                onResume();
+                            }
+                        })
+                .setNegativeButton("New Game",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int i) {
+                                finish();
+                            }
+                        });
+
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
+    }
+    private void gameOver() {
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(bonus_game.this);
+        alertDialogBuilder
+                .setMessage("Game Over ! Your Score is :" + score)
+                .setCancelable(true)
+                .setPositiveButton("Try Agin",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                startActivity( new Intent(getApplicationContext(),bonus_game.class));
+                            }
+                        });
+
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
     }
 }
---
+++ app/src/main/java/com/example/BrianStrom/questions.java
@@ -0,0 +1,63 @@
+package com.example.BrianStrom;
+
+public class questions {
+
+    public String mQuestions[]={
+            "which is the first planent in the Solar system?",
+            "which is the second planent in the Solar system?",
+            "which is the Third planent in the Solar system?",
+            "which is the Fourth planent in the Solar system?",
+            "which is the Fifth planent in the Solar system?",
+            "which is the Sixth planent in the Solar system?",
+            "which is the Seventh planent in the Solar system?",
+            "which is the Eight planent in the Solar system?",
+            "which is the Nineth planent in the Solar system?"
+    };
+
+
+    public String answers[][]={
+            {"Mercury","Venus","Mars","Saturn"},
+            {"Mercury","Venus","Mars","Saturn"},
+            {"Mercury","Venus","Mars","Saturn"},
+            {"Mercury","Venus","Mars","Saturn"},
+            {"Jupiter","Venus","Mars","Saturn"},
+            {"Mercury","Venus","Saturn","Saturn"},
+            {"Mercury","Venus","Mars","Uranus"},
+            {"Mercury","Neptune","Mars","Saturn"},
+            {"Mercury","Venus","Pluto","Saturn"}
+
+    };
+
+    private String correctAnswer []={"Mercury","Venus","Earth","Mars","Jupiter","Saturn","Uranus","Neptune","Pluto"};
+
+    public String getQuestion(int a){
+        String question = mQuestions[a];
+        return question;
+    }
+
+    public String getChoice1(int a){
+        String choice = answers[a][0];
+        return choice;
+    }
+
+    public String getChoice2(int a){
+        String choice = answers[a][1];
+        return choice;
+    }
+
+    public String getChoice3(int a){
+        String choice = answers[a][2];
+        return choice;
+    }
+
+    public String getChoice4(int a){
+        String choice = answers[a][3];
+        return choice;
+    }
+
+    public String getCorrectAnswer(int a){
+        String Canswer = correctAnswer[a];
+        return Canswer;
+    }
+
+}
